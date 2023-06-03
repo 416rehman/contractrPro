54d707213cbba1b560cf80b4f9c24968cc540bbd
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-06-03 18:46:12 -0400

    removed comment.js for this branch

--- api/models/comment.js
+++
@@ -1,7 +0,0 @@
-module.exports.comment = (sequelize, DataTypes) => {
-    return sequelize.define("Comment", {
-        comment_text: DataTypes.STRING(1024),
-        attachments: DataTypes.STRING(512),
-        created_by: DataTypes.INTEGER(),
-    });
-}
