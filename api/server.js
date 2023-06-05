a78244cbe6afced50838ed880ed1fcb8a60e4c0c
Cleo <90213619+Cleoxia@users.noreply.github.com>
2023-06-05 15:55:40 -0400

    Prettier eslint logging (#254)
    * Add prettier
    
    * Add prettier
    
    * Add prettier
    
    * Add eslint
    
    * add Eslint, add pino logger
    
    * add logger and pino in server.js
    
    * fix issues, delete extra file accidently created

--- api/server.js
+++ api/server.js
@@ -2,38 +2,49 @@
  * Model: https://www.figma.com/file/oXjOLhFELvDrGkTz8BIJUI/Untitled
  */
 
-require('dotenv').config()
+require('dotenv').config();
 const cors = require('cors');
 const express = require('express'),
-    routes = require('./routes'),
-    {sequelize} = require('./models')
-const {tokenAuthHandler} = require("./middleware/auth-middleware");
+  routes = require('./routes'),
+  { sequelize } = require('./models');
+const { tokenAuthHandler } = require('./middleware/auth-middleware');
+const logger = require('./logger');
+const pino = require('pino-http')({
+  // Use our default logger instance, which is already configured
+  logger,
+});
 
-const port = process.env.PORT || 3000
-const app = express()
+const port = process.env.PORT || 3000;
+const app = express();
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Use logging middleware
+app.use(pino);
 
 app.use((req, res, next) => {
-    const path = (__filename.split('/').slice(-1)[0]).split('\\').slice(-1)[0]
-    res.set('Router', path)
-    next()
-})
+  const path = __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0];
+  res.set('Router', path);
+  next();
+});
 
 app.get('/', (req, res) => {
-    res.send('Connected!')
-})
+  res.send('Connected!');
+});
 
-app.use('/auth', routes.auth)
-app.use(tokenAuthHandler)   // security middleware, uses tokenAuthHandler to authenticate via access tokens in the authorization header
-app.use('/users/', routes.user)
-app.use('/organizations/',  routes.organization)
+app.use('/auth', routes.auth);
+app.use(tokenAuthHandler); // security middleware, uses tokenAuthHandler to authenticate via access tokens in the authorization header
+app.use('/users/', routes.user);
+app.use('/organizations/', routes.organization);
 
-sequelize.sync().then(() => {
+sequelize
+  .sync()
+  .then(() => {
     app.listen(port, () => {
-        console.log(`listening at http://localhost:${port}/`)
-    })
-}).catch(err => {
-    console.log(err)
-})
+      console.log(`listening at http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
