4778fb32bae64101a9c2c68e0f007df54260cd10
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-06-19 19:29:58 -0400

    Implemented the route for getting a user's organizations

--- api/routes/user/getUserOrganizations.js
+++ api/routes/user/getUserOrganizations.js
@@ -1,6 +1,31 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { Organization, User } = require('../../db');
+
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../utils/response');
+
+// Gets the selected user's organizations
+module.exports = async (req, res) => {
+
+    try {
+        const userID = req.params.user_id;
+
+        if (!userID) return res.status(400).json(createErrorResponse('User ID is required'));
+        
+        const userOrganizations = await User.findAll({
+            where: {
+                id: userID,
+            },
+            include: {
+                model: Organization,
+            }
+        });
+        
+        return res.status(200).json(createSuccessResponse(userOrganizations));
+    }
+    catch (error) {
+        res.status(500).json(createErrorResponse(error.message, error));
+    }
+
 }
