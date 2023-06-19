fb0e0957f84a01d4e0da8708a5be2b8bde87672f
416rehman <15690606+416rehman@users.noreply.github.com>
2023-06-19 15:12:45 -0400

    Refactor organization routes to update and create organizations with proper error handling and response messages.

--- api/routes/organization/putOrganization.js
+++ api/routes/organization/putOrganization.js
@@ -1,6 +1,31 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { Organization, sequelize } = require('../../db')
+const { createErrorResponse, createSuccessResponse } = require('../../utils/response')
+
+// Updates an organization
+module.exports = async (req, res) => {
+    try {
+        const body = {
+            ...(req.body),
+            updatedByUserId: req.auth.id,
+        }
+
+        const orgId = req.params.org_id
+        if (!orgId) {
+            return res.status(400).json(createErrorResponse('Organization id is required'))
+        }
+
+        await sequelize.transaction(async (transaction) => {
+            const updatedOrg = await Organization.update(body, {
+                where: {
+                    id: orgId,
+                },
+                transaction,
+                returning: true,
+            })
+
+            return res.status(200).json(createSuccessResponse(updatedOrg))
+        })
+    } catch (error) {
+        return res.status(500).json(createErrorResponse(error.message, error))
+    }
 }
