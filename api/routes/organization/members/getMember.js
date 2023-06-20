5e12658704cf771f7de7b3947a5c5bc412fc3322
Cleo <cleoxia1@gmail.com>
2023-06-20 01:37:46 -0400

    init

--- api/routes/organization/members/getMember.js
+++ api/routes/organization/members/getMember.js
@@ -1,6 +1,49 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+/**
+ * const { Organization,User } = require('../../db')
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../utils/response')
+*/
+
+//Retrieve user by id of user from the same organization
+/**
+ *module.exports = async(req, res) => {
+    try{
+        const userId = req.params.user_id;
+        const orgId = req.params.org_id;
+
+        //check orgId and userId input
+        if (!orgId) {
+            return res.status(400).json(createErrorResponse('Organization id is required'));
+        }
+        if (!userId) {
+            return res.status(400).json(createErrorResponse('user id is required'));
+        }
+
+        //since user has unique id, it only return 1 user object
+        const user = await User.findAll({
+            where: { id: userId },
+            include: [
+                {
+                    model: Organization,
+                    where: { id: orgId },
+
+                    //if any attributes need to be used by front-end ,specify them here
+                    attributes: ['id', 'username', 'email', 'phone'],
+                },
+            ],
+        });
+
+        //if no user in such organization, return empty array
+        if (!user) {
+            return [];
+        }
+
+        res.status(200).json(createSuccessResponse(user));
+
+    }catch(err){
+        res.status(500).json(createErrorResponse(error.message, err));
+    }
 }
+ */
--- api/routes/organization/members/getMembers.js
+++ api/routes/organization/members/getMembers.js
@@ -1,6 +1,41 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+/**
+ * const { Organization,User } = require('../../db')
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../utils/response')
+*/
+
+//Retrieve users by organization display their id, username,email,phone.
+/**
+ * 
+ module.exports = async(req, res) => {
+    try{
+        const orgId = req.params.org_id;
+
+        //check orgId input
+        if (!orgId) {
+            return res.status(400).json(createErrorResponse('Organization id is required'));
+        }
+
+        const users = await User.findByPk(orgId,{
+            include: [
+                {
+                    model: User,
+                    //user id ,username, email and phone has been retrieved and ready to be used by front-end
+                    attributes: ['id', 'username', 'email', 'phone'], 
+                },
+            ], 
+        });
+
+        //if no user in organization at all, return empty array
+        if (!users) {
+            return [];
+        }
+        res.status(200).json(createSuccessResponse(users));
+
+    }catch(err){
+        res.status(500).json(createErrorResponse(error.message, err));
+    }
 }
+ */
