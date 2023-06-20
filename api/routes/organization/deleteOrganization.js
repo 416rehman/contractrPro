1d5a387d2ded6d19ee3c53b842053626d28c2431
jihee10 <jihee10@users.noreply.github.com>
2023-06-20 04:11:40 +0000

    Apply formatting changes

--- api/routes/organization/deleteOrganization.js
+++ api/routes/organization/deleteOrganization.js
@@ -5,9 +5,9 @@ const {
 } = require('../../utils/response')
 
 module.exports = async (req, res) => {
-    try{
-        const org_id = req.params.org_id;
-        const organization = await Organization.findByIdAndDelete(org_id);
+    try {
+        const org_id = req.params.org_id
+        const organization = await Organization.findByIdAndDelete(org_id)
 
         if (!organization) {
             res.status(404).send({
@@ -16,8 +16,8 @@ module.exports = async (req, res) => {
             return
         }
 
-        res.status(200).json(createSuccessResponse(organization));
-    }catch(error){
-        res.status(500).json(createErrorResponse(error.message, error));
+        res.status(200).json(createSuccessResponse(organization))
+    } catch (error) {
+        res.status(500).json(createErrorResponse(error.message, error))
     }
 }
