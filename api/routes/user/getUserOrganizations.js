bf39465c5c854f9fa5b605c7240092a1a3341f40
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-06-19 20:49:51 -0400

    Updated the implementation to include transactions and exclude most of the User's attributes when retrieving their organizations

--- api/routes/user/getUserOrganizations.js
+++ api/routes/user/getUserOrganizations.js
@@ -1,28 +1,35 @@
-const { Organization, User } = require('../../db');
+const { sequelize, Organization, User } = require('../../db');
 
-const {
-    createSuccessResponse,
-    createErrorResponse,
-} = require('../../utils/response');
+const { createSuccessResponse, createErrorResponse } = require('../../utils/response');
 
-// Gets the a user's organizations
+// Gets a user's organizations
 module.exports = async (req, res) => {
 
     try {
         const userID = req.params.user_id;
 
-        if (!userID) return res.status(400).json(createErrorResponse('User ID is required'));
-        
-        const userOrganizations = await User.findAll({
-            where: {
-                id: userID,
-            },
-            include: {
-                model: Organization,
-            }
-        });
-        
-        return res.status(200).json(createSuccessResponse(userOrganizations));
+        if (!userID) {
+            return res.status(400).json(createErrorResponse('User ID is required'));
+        }
+
+        await sequelize.transaction(async (transaction) => {
+            
+            const userOrganizations = await User.findAll({
+                attributes: {
+                    exclude: ['username', 'fullName', 'email', 'phone', 'password', 'avatarUrl', 'refreshToken', 'createdAt', 'updatedAt', 'deletedAt', 'updatedByUserId'],
+                },
+                where: {
+                    id: userID,
+                },
+                include: {
+                    model: Organization,
+                },
+                transaction,
+            });
+
+            return res.status(200).json(createSuccessResponse(userOrganizations));
+
+        }); 
     }
     catch (error) {
         res.status(500).json(createErrorResponse(error.message, error));
