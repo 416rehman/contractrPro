46b6c75228c16d1b3a85769aba4bc073309fa082
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-06-19 23:50:37 -0400

    Update deleteOrganization.js
    DELETE route for /organizations/:org_id to delete an organization by ID

--- api/routes/organization/deleteOrganization.js
+++ api/routes/organization/deleteOrganization.js
@@ -1,6 +1,23 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { Organization } = require('../../db')
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../utils/response')
+
+module.exports = async (req, res) => {
+    try{
+        const org_id = req.params.org_id;
+        const organization = await Organization.findByIdAndDelete(org_id);
+
+        if (!organization) {
+            res.status(404).send({
+                message: `Organization with ID ${org_id} was not found`,
+            })
+            return
+        }
+
+        res.status(200).json(createSuccessResponse(organization));
+    }catch(err){
+        res.status(500).json(createErrorResponse(error.message, error));
+    }
 }
