e1f02acd47f053b34729e682e01e3ff037f3d096
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-06-19 23:55:29 -0400

    Update leaveOrganization.js
    delete route for /organizations/:org_id/members/:member_id to have a user leave the organization by ID

--- api/routes/user/leaveOrganization.js
+++ api/routes/user/leaveOrganization.js
@@ -1,6 +1,61 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { sequelize, Organization, User } = require('../../db')
+
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../utils/response')
+
+module.exports = async (req, res) => {
+    try {
+        const userID = req.params.user_id
+        const organizationID = req.params.org_id
+
+        if (!userID) {
+            return res
+                .status(400)
+                .json(createErrorResponse('User ID is required'))
+        }
+
+        if (!organizationID) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Organization ID is required'))
+        }
+
+        await sequelize.transaction(async (transaction) => {
+            const user = await User.findOne({
+                where: {
+                    id: userID,
+                },
+                transaction,
+            })
+
+            if (!user) {
+                return res
+                    .status(404)
+                    .json(createErrorResponse('User not found'))
+            }
+
+            const organization = await Organization.findOne({
+                where: {
+                    id: organizationID,
+                },
+                transaction,
+            })
+
+            if (!organization) {
+                return res
+                    .status(404)
+                    .json(createErrorResponse('Organization not found'))
+            }
+
+            await user.removeOrganization(organization, { transaction })
+
+            return res
+                .status(200)
+                .json(createSuccessResponse('Leave organization successfully'))
+        })
+    } catch (error) {
+        res.status(500).json(createErrorResponse(error.message, error))
+    }
 }
