3a4eb5e1e608181c9a7ec2b7e57d058a974ac9c2
Ibrahem Elsawy <79288021+IbrahemElsawy@users.noreply.github.com>
2023-06-19 21:04:35 -0400

    Feat/GetUserOrganizations (#265)
    * Updated the comment for the GET route of a user's organizations from POST to GET
    
    * Implemented the route for getting a user's organizations
    
    * Implemented the route for getting a user's organizations
    
    * Updated the implementation to include transactions and exclude most of the User's attributes when retrieving their organizations
    
    * Apply formatting changes
    
    ---------
    
    Co-authored-by: IbrahemElsawy <IbrahemElsawy@users.noreply.github.com>

--- api/routes/user/getUserOrganizations.js
+++ api/routes/user/getUserOrganizations.js
@@ -1,6 +1,52 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { sequelize, Organization, User } = require('../../db')
+
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../utils/response')
+
+// Gets a user's organizations
+module.exports = async (req, res) => {
+    try {
+        const userID = req.params.user_id
+
+        if (!userID) {
+            return res
+                .status(400)
+                .json(createErrorResponse('User ID is required'))
+        }
+
+        await sequelize.transaction(async (transaction) => {
+            const userOrganizations = await User.findAll({
+                attributes: {
+                    exclude: [
+                        'username',
+                        'fullName',
+                        'email',
+                        'phone',
+                        'password',
+                        'avatarUrl',
+                        'refreshToken',
+                        'createdAt',
+                        'updatedAt',
+                        'deletedAt',
+                        'updatedByUserId',
+                    ],
+                },
+                where: {
+                    id: userID,
+                },
+                include: {
+                    model: Organization,
+                },
+                transaction,
+            })
+
+            return res
+                .status(200)
+                .json(createSuccessResponse(userOrganizations))
+        })
+    } catch (error) {
+        res.status(500).json(createErrorResponse(error.message, error))
+    }
 }
