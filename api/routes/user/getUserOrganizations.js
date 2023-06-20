1c438485ffc8aacc4925fe554796f0623ee7bf5f
IbrahemElsawy <IbrahemElsawy@users.noreply.github.com>
2023-06-20 00:51:48 +0000

    Apply formatting changes

--- api/routes/user/getUserOrganizations.js
+++ api/routes/user/getUserOrganizations.js
@@ -1,22 +1,37 @@
-const { sequelize, Organization, User } = require('../../db');
+const { sequelize, Organization, User } = require('../../db')
 
-const { createSuccessResponse, createErrorResponse } = require('../../utils/response');
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../utils/response')
 
 // Gets a user's organizations
 module.exports = async (req, res) => {
-
     try {
-        const userID = req.params.user_id;
+        const userID = req.params.user_id
 
         if (!userID) {
-            return res.status(400).json(createErrorResponse('User ID is required'));
+            return res
+                .status(400)
+                .json(createErrorResponse('User ID is required'))
         }
 
         await sequelize.transaction(async (transaction) => {
-            
             const userOrganizations = await User.findAll({
                 attributes: {
-                    exclude: ['username', 'fullName', 'email', 'phone', 'password', 'avatarUrl', 'refreshToken', 'createdAt', 'updatedAt', 'deletedAt', 'updatedByUserId'],
+                    exclude: [
+                        'username',
+                        'fullName',
+                        'email',
+                        'phone',
+                        'password',
+                        'avatarUrl',
+                        'refreshToken',
+                        'createdAt',
+                        'updatedAt',
+                        'deletedAt',
+                        'updatedByUserId',
+                    ],
                 },
                 where: {
                     id: userID,
@@ -25,14 +40,13 @@ module.exports = async (req, res) => {
                     model: Organization,
                 },
                 transaction,
-            });
+            })
 
-            return res.status(200).json(createSuccessResponse(userOrganizations));
-
-        }); 
+            return res
+                .status(200)
+                .json(createSuccessResponse(userOrganizations))
+        })
+    } catch (error) {
+        res.status(500).json(createErrorResponse(error.message, error))
     }
-    catch (error) {
-        res.status(500).json(createErrorResponse(error.message, error));
-    }
-
 }
