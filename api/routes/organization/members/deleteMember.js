ca9a03ba40b6453c07ffc599fb35e644d6b6e931
416rehman <15690606+416rehman@users.noreply.github.com>
2023-06-20 02:55:46 -0400

    implemented add and delete member

--- api/routes/organization/members/deleteMember.js
+++ api/routes/organization/members/deleteMember.js
@@ -1,6 +1,34 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { OrganizationMember, Organization, sequelize } = require('../../../db')
+const { createErrorResponse, createSuccessResponse } = require('../../../utils/response')
+module.exports = async (req, res) => {
+    try {
+        if (!req.params.org_id) {
+            return res.status(400).json(createErrorResponse('Organization ID is required'))
+        }
+        if (!req.params.member_id) {
+            return res.status(400).json(createErrorResponse('Member ID is required'))
+        }
+
+        await sequelize.transaction(async (transaction) => {
+            const member = await OrganizationMember.findOne({
+                where: {
+                    OrganizationId: req.params.org_id,
+                    id: req.params.member_id,
+                },
+                transaction,
+            })
+
+            if (!member) {
+                throw new Error('Member not found')
+            }
+
+            await member.destroy({
+                transaction,
+            })
+
+            res.status(200).json(createSuccessResponse(member))
+        })
+    } catch (error) {
+        res.status(500).json(createErrorResponse(error.message, error))
+    }
 }
