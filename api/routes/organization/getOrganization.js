e82db5d5f83a158d5d7ffa104f4844303bd3a855
Cleo <cleoxia1@gmail.com>
2023-06-19 23:21:03 -0400

    get orgnizations by organization id

--- api/routes/organization/getOrganization.js
+++ api/routes/organization/getOrganization.js
@@ -1,6 +1,21 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { Organization } = require('../../db')
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../utils/response')
+
+//retrieve all organization
+module.exports = async (req, res) => {
+    try{
+        const ownerId = req.auth.id;
+
+        //since organization has unique ids, it only return 1 organization object
+        const organizations = await Organization.findAll({
+            where: { ownerId },
+        });
+
+        res.status(200).json(createSuccessResponse(organizations));
+    }catch(err){
+        res.status(500).json(createErrorResponse(error.message, error));
+    }
 }
