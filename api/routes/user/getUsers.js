301451655e0a9345c4e82fc995c8b3e9b7a71316
Cleo <cleoxia1@gmail.com>
2023-06-20 01:40:53 -0400

    fix error

--- api/routes/user/getUsers.js
+++ api/routes/user/getUsers.js
@@ -8,48 +8,17 @@ const {
 module.exports = async (req, res) => {
     try {
         const users = await User.findAll();
+
         //if no user in organization at all, return empty array
         if (!users) {
             return [];
         }
         res.status(200).json(createSuccessResponse(users));
     } catch (err) {
-        res.status(500).json(createErrorResponse(error.message, err));
+        res.status(500).json(createErrorResponse(err.message, err));
     }
 };
 
 
 
-//Retrieve users by organization display their id, username,email,phone.
-/**
- * 
- module.exports = async(req, res) => {
-    try{
-        const orgId = req.params.org_id;
 
-        //check orgId input
-        if (!orgId) {
-            return res.status(400).json(createErrorResponse('Organization id is required'));
-        }
-
-        const users = await User.findByPk(orgId,{
-            include: [
-                {
-                    model: User,
-                    //user id ,username, email and phone has been retrieved and ready to be used by front-end
-                    attributes: ['id', 'username', 'email', 'phone'], 
-                },
-            ], 
-        });
-
-        //if no user in organization at all, return empty array
-        if (!users) {
-            return [];
-        }
-        res.status(200).json(createSuccessResponse(users));
-
-    }catch(err){
-        res.status(500).json(createErrorResponse(error.message, err));
-    }
-}
- */
