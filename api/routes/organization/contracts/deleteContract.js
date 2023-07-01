7337df16fc213390635e86019fa97be7424a9573
Cleo <cleoxia1@gmail.com>
2023-07-01 19:33:45 -0400

    fix error of deleteContract

--- api/routes/organization/contracts/deleteContract.js
+++ api/routes/organization/contracts/deleteContract.js
@@ -1,46 +1,48 @@
 //*******************************************TODO******************* */
-const { sequelize, Contract } = require('../../../db');
+const { sequelize, Contract, Expense, Invoice, Job } = require('../../../db');
 const {
     createSuccessResponse,
     createErrorResponse,
 } = require('../../../utils/response');
 module.exports = async(req, res) => {
+    const contractId=req.params.contract_id;
     try{
         if (!req.params.org_id) {
             return res
                 .status(400)
-                .json(createErrorResponse('Organization ID is required'));
+                .json(createErrorResponse('Organization ID required'));
         }
-        if (!req.params.client_id) {
-            return res
-                .status(400)
-                .json(createErrorResponse('Client ID is required'));
-        }
+        
         if (!req.params.contract_id) {
             return res
                 .status(400)
-                .json(createErrorResponse('Contract ID is required'));
+                .json(createErrorResponse('Contract ID required'));
         }
 
         await sequelize.transaction(async (transaction) => {
             const contract = await Contract.findOne({
                 where: {
                     OrganizationId: req.params.org_id,
-                    ClientId: req.params.client_id,
-                    id: req.params.contract_id,
+                    id: contractId,
                 },
                 transaction,
             });
 
             if (!contract) {
-                throw new Error('contract not exist');
+                throw new Error(`contract ${contractId} not exist`);
             }
 
-            await contract.destroy({
-                transaction,
-            });
+            await Promise.all([
+                Expense.destroy({ where: { ContractId: contractId }, transaction }),
+                Invoice.destroy({ where: { ContractId: contractId }, transaction }),
+                Job.destroy({ where: { ContractId: contractId }, transaction }),
+                //Comment.destroy({ where: { ContractId: contractId }, transaction }), if there is a comment??
+            ]);
 
-            return res.status(200).json(createSuccessResponse(contract));
+            // Delete the contract
+            await contract.destroy({ transaction });
+
+            return res.status(200).json(createSuccessResponse(`Contract ${contractId} and related data has been deleted successfully`));
         });
 
     }catch(err){
