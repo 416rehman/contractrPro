b276b806112cad45fa585b69024f4ac84d9b0219
Cleo <cleoxia1@gmail.com>
2023-07-01 18:08:56 -0400

    deleteContract init

--- api/routes/organization/contracts/deleteContract.js
+++ api/routes/organization/contracts/deleteContract.js
@@ -1,7 +1,50 @@
 //*******************************************TODO******************* */
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { sequelize, Contract } = require('../../../db');
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../../utils/response');
+module.exports = async(req, res) => {
+    try{
+        if (!req.params.org_id) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Organization ID is required'));
+        }
+        if (!req.params.client_id) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Member ID is required'));
+        }
+        if (!req.params.contract_id) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Member ID is required'));
+        }
+
+        await sequelize.transaction(async (transaction) => {
+            const contract = await Contract.findOne({
+                where: {
+                    OrganizationId: req.params.org_id,
+                    ClientId: req.params.client_id,
+                    id: req.params.member_id,
+                },
+                transaction,
+            });
+
+            if (!contract) {
+                throw new Error('contract not exist')
+            }
+
+            await contract.destroy({
+                transaction,
+            });
+
+            return res.status(200).json(createSuccessResponse(contract));
+        });
+
+    }catch(err){
+         return res.status(500).json(createErrorResponse(err.message));
+
+    }
 }
