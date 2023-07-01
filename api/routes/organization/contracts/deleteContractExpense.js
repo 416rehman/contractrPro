58f5e5f09e1f22862eb166f067fb69f1075a198a
Cleo <cleoxia1@gmail.com>
2023-07-01 18:27:19 -0400

    deleteContractExpense init

--- api/routes/organization/contracts/deleteContractExpense.js
+++ api/routes/organization/contracts/deleteContractExpense.js
@@ -1,7 +1,62 @@
 //*******************************************TODO******************* */
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { sequelize, Expense } = require('../../../db');
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../../utils/response');
+module.exports = async(req, res) => {
+    try{
+        if (!req.params.org_id) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Organization ID is required'));
+        }
+        if (!req.params.contract_id) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Contract ID is required'));
+        }
+        if (!req.params.job_id) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Job ID is required'));
+        }
+        if (!req.params.vendor_id) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Vendor ID is required'));
+        }
+        if (!req.params.expense_id) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Expense ID is required'));
+        }
+
+        await sequelize.transaction(async (transaction) => {
+            const expense = await Expense.findOne({
+                where: {
+                    OrganizationId: req.params.org_id,
+                    ContractId: req.params.contract_id,
+                    JobId: req.params.job_id,
+                    VendorId: req.params.vendor_id,
+                    id: req.params.expense_id,
+                },
+                transaction,
+            });
+
+            if (!expense) {
+                throw new Error('contract not exist');
+            }
+
+            await expense.destroy({
+                transaction,
+            });
+
+            return res.status(200).json(createSuccessResponse(expense));
+        });
+
+    }catch(err){
+         return res.status(500).json(createErrorResponse(err.message));
+
+    }
 }
