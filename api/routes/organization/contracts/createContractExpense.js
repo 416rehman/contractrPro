f49a058c0b3c651ba1eb74dfd329f7eda018a9b9
Cleo <cleoxia1@gmail.com>
2023-07-01 17:45:17 -0400

    createContractExpense init

--- api/routes/organization/contracts/createContractExpense.js
+++ api/routes/organization/contracts/createContractExpense.js
@@ -1,6 +1,38 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+//*******************************************TODO******************* */
+const { sequelize, Expense } = require('../../db');
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../../utils');
+const { pick } = require('../../../utils');
+
+module.exports = async(req, res) => {
+    try{
+        const orgId = req.params.org_id;
+        if (!orgId) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Organization ID is required'))
+        }
+        const body = {
+            ...pick(req.body, [
+                'description',
+                'date',
+            ]),
+            OrganizationId: orgId,
+            ContractId:req.params.org_id,
+            JobId:req.params.job_id,
+            VendorId:req.params.vendor_id,
+            updatedByUserId: req.auth.id,
+        };
+        await sequelize.transaction(async (transaction) => {
+            const expense = Expense.build(body);
+            await expense.save({ transaction });
+
+            res.status(200).json(createSuccessResponse(expense));
+        })
+        
+    }catch(err){
+        res.status(500).json(createErrorResponse(err.message));
+    }
 }
