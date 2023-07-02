92333a76935a9a22f4d38823f53c93a0be3e4e5f
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-07-01 23:45:18 -0400

    Update deleteJobExpense.js

--- api/routes/organization/contracts/jobs/deleteJobExpense.js
+++ api/routes/organization/contracts/jobs/deleteJobExpense.js
@@ -1,6 +1,44 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { sequelize, JobExpense, Job } = require('../../../db')
+const { createSuccessResponse, createErrorResponse } = require('../../../utils/response')
+const { validator } = require('../../../utils/validator')
+
+// Deletes the organization's contract job expense
+module.exports = async (req, res) => {
+            
+try {
+            
+    const jobExpenseID = req.params.job_expense_id;
+            
+    if (!jobExpenseID || !validator(jobExpenseID)) {
+        return res
+            .status(400)
+            .json(createErrorResponse('Job Expense ID required'))
+    }
+            
+    await sequelize.transaction(async (transaction) => {
+            
+        const jobExpense = await JobExpense.findOne({
+            where: {
+                id: jobExpenseID,
+            },
+            transaction,
+        })
+            
+        if (!jobExpense) {
+            return res
+            .status(400)
+            .json(createErrorResponse('Job Expense not found'))
+            }
+            
+            await jobExpense.destroy({
+                transaction,
+            })
+            
+            return res.status(201).json(createSuccessResponse('Job Expense deleted successfully'))
+        })
+            
+    } catch (error) {
+        return res.status(500).json(createErrorResponse(error.message))
+    }
+            
 }
