04e5d78db12a25144670b2bb530b2d512837767c
Cleo <cleoxia1@gmail.com>
2023-07-01 23:11:16 -0400

    add update routes for updateInvoice, updateExpense, updateContract

--- api/routes/organization/contracts/updateContract.js
+++ api/routes/organization/contracts/updateContract.js
@@ -1,7 +1,48 @@
 //*******************************************TODO******************* */
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+//PUT /organizations/:org_id/contracts/:contract_id 
+const { sequelize, Contract } = require('../../../db');
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../../utils/response');
+const { pick } = require('../../../utils');
+module.exports = async(req, res) => {
+    const orgId = req.params.org_id;
+    const contractId = req.params.contract_id;
+    try{
+        const body = {
+            ...pick(req.body, [
+                'name',
+                'description',
+                'startDate',
+                'dueDate',
+                'completionDate',
+                'status'
+            ]),
+            updatedByUserId: req.auth.id,
+        };
+
+        await sequelize.transaction(async (transaction) => {
+            const contract = await Contract.findOne({
+                where: { 
+                    id: contractId, 
+                    OrganizationId: orgId,
+                },
+                transaction,
+                returning: true,
+            });
+
+            if (!contract) {
+                return res.status(404).json(createErrorResponse(`contract ${contractId} not exist`));
+            }
+
+            // Update the contract
+            await contract.update(body, { transaction });
+
+            return res.status(200).json(createSuccessResponse(`Contract ${contractId} updated successfully`));
+        });
+
+    }catch(err){
+        return res.status(500).json(createErrorResponse(err.message));
+    }
 }
--- api/routes/organization/contracts/updateContractExpense.js
+++ api/routes/organization/contracts/updateContractExpense.js
@@ -1,7 +1,43 @@
 //*******************************************TODO******************* */
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+// PUT /organizations/:org_id/contracts/:contract_id/expenses/:expense_id 
+const { sequelize, Expense } = require('../../../db');
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../../utils/response');
+const { pick } = require('../../../utils');
+
+module.exports = async(req, res) => {
+    const orgId = req.params.org_id;
+    const contractId = req.params.contract_id;
+    const expenseId = req.params.expense_id;
+    try {
+        const body = {
+            ...pick(req.body, [
+                'description',
+                'date',
+            ]),
+            updatedByUserId: req.auth.id,
+        }
+        
+
+        await sequelize.transaction(async (transaction) => {
+            // Find the expense
+            const expense = await Expense.findOne({
+                where: { id: expenseId, ContractId: contractId, OrganizationId: orgId },
+                transaction,
+            });
+
+            if (!expense) {
+                return res.status(404).json(createErrorResponse(`Expense ${expenseId} not exist`));
+            }
+
+            // Update the expense
+            await expense.update(body, { transaction });
+
+            return res.status(200).json(createSuccessResponse(`Expense ${expenseId} and related entries updated successfully`));
+        });
+    } catch (error) {
+        return res.status(500).json(createErrorResponse(error.message));
+    }
 }
