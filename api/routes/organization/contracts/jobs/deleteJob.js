b45f4f1150235403027bbf1664c369a1709f9662
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-07-01 23:32:08 -0400

    Update deleteJob.js

--- api/routes/organization/contracts/jobs/deleteJob.js
+++ api/routes/organization/contracts/jobs/deleteJob.js
@@ -1,6 +1,44 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
-}
+const { sequelize, Job, Contract } = require('../../../db')
+const { createSuccessResponse, createErrorResponse } = require('../../../utils/response')
+const { validator } = require('../../../utils/validator')
+
+// Deletes the organization's contract job
+module.exports = async (req, res) => {
+    
+                try {
+    
+                    const jobID = req.params.job_id;
+    
+                    if (!jobID || !validator(jobID)) {
+                        return res
+                            .status(400)
+                            .json(createErrorResponse('Job ID required'))
+                    }
+    
+                    await sequelize.transaction(async (transaction) => {
+    
+                        const job = await Job.findOne({
+                            where: {
+                                id: jobID,
+                            },
+                            transaction,
+                        })
+    
+                        if (!job) {
+                            return res
+                            .status(400)
+                            .json(createErrorResponse('Job not found'))
+                        }
+    
+                        await job.destroy({
+                            transaction,
+                        })
+    
+                        return res.status(201).json(createSuccessResponse('Job deleted successfully'))
+                    })
+    
+                } catch (error) {
+                    return res.status(500).json(createErrorResponse(error.message))
+                }
+    
+        }
