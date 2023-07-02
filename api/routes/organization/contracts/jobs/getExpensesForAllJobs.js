bf65ddc46c67407d3ea2d11fb33cf99b7db5fa6f
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-07-01 23:49:29 -0400

    Update getExpensesForAllJobs.js

--- api/routes/organization/contracts/jobs/getExpensesForAllJobs.js
+++ api/routes/organization/contracts/jobs/getExpensesForAllJobs.js
@@ -1,6 +1,35 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
-}
+const { sequelize, JobExpense, Job } = require('../../../db')
+const { createSuccessResponse, createErrorResponse } = require('../../../utils/response')
+const { validator } = require('../../../utils/validator')
+
+// Gets all the organization's contract job expenses
+module.exports = async (req, res) => {
+                try {
+    
+                    const contractID = req.params.contract_id;
+    
+                    if (!contractID || !validator(contractID)) {
+                        return res
+                            .status(400)
+                            .json(createErrorResponse('Contract ID required'))
+                    }
+    
+                    const jobs = await Job.findAll({
+                        where: {
+                            contract_id: contractID,
+                        },
+                        include: [
+                            {
+                                model: JobExpense,
+                                as: 'expenses',
+                            },
+                        ],
+                    })
+    
+                    return res.status(201).json(createSuccessResponse(jobs))
+    
+                } catch (error) {
+                    return res.status(500).json(createErrorResponse(error.message))
+                }
+    
+        }
