a88b88a85effb39e71e2db01d15483f54aa06891
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-07-02 17:13:48 -0400

    Update getJobMembers.js
    Gets all the organization's job members for a specific job

--- api/routes/organization/contracts/jobs/getJobMembers.js
+++ api/routes/organization/contracts/jobs/getJobMembers.js
@@ -1,6 +1,40 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { sequelize, JobMember, Job } = require('../../../db')
+const { createSuccessResponse, createErrorResponse } = require('../../../utils/response')
+const { validator } = require('../../../utils/validator')
+
+//Get all the organization's job members for a specific job
+module.exports = async (req, res) => {
+    try {
+        const orgID = req.params.org_id;
+        const contractID = req.params.contract_id;
+        const jobID = req.params.job_id;
+
+        if (!orgID || !validator(orgID) || !contractID || !validator(contractID) || !jobID || !validator(jobID)) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Invalid organization, contract, or job ID'))
+        }
+
+        const members = await JobMember.findAll({
+            where: {
+                organizationId: orgID,
+                contractId: contractID,
+                jobId: jobID
+            },
+            include: [
+                {
+                    model: Job,
+                    where: {
+                        organizationId: orgID,
+                        contractId: contractID,
+                        id: jobID
+                    }
+                }
+            ]
+        })
+
+        return res.status(200).json(createSuccessResponse(members))
+    } catch (error) {
+        return res.status(500).json(createErrorResponse(error.message))
+    }
 }
