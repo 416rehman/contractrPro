047c1c6b086a56b514fe9f38c4835a028131db30
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-07-02 17:02:40 -0400

    Update getJob.js
    Gets a specific job for a specific contract

--- api/routes/organization/contracts/jobs/getJob.js
+++ api/routes/organization/contracts/jobs/getJob.js
@@ -1,6 +1,30 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { sequelize, Job } = require('../../../db')
+const { createSuccessResponse, createErrorResponse } = require('../../../utils/response')
+const { validator } = require('../../../utils/validator')
+
+// Gets a specific job for a specific contract
+module.exports = async (req, res) => {
+    try {
+        const orgID = req.params.org_id;
+        const contractID = req.params.contract_id;
+        const jobID = req.params.job_id;
+
+        if (!orgID || !validator(orgID) || !contractID || !validator(contractID) || !jobID || !validator(jobID)) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Invalid organization, contract, or job ID'))
+        }
+
+        const job = await Job.findOne({
+            where: {
+                organizationId: orgID,
+                contractId: contractID,
+                id: jobID
+            }
+        })
+
+        return res.status(200).json(createSuccessResponse(job))
+    } catch (error) {
+        return res.status(500).json(createErrorResponse(error.message))
+    }
 }
