0b7b71e81048991facec6d0ac0624147cead15c7
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-07-02 16:44:30 -0400

    Update getExpensesForJob.js
    Gets all the organization's contract expenses for a specific job

--- api/routes/organization/contracts/jobs/getExpensesForJob.js
+++ api/routes/organization/contracts/jobs/getExpensesForJob.js
@@ -1,6 +1,40 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { sequelize, JobExpense, Job } = require('../../../db')
+const { createSuccessResponse, createErrorResponse } = require('../../../utils/response')
+const { validator } = require('../../../utils/validator')
+
+// Gets all the organization's contract expenses for a specific job
+module.exports = async (req, res) => {
+    try {
+        const orgID = req.params.org_id;
+        const contractID = req.params.contract_id;
+        const jobID = req.params.job_id;
+
+        if (!orgID || !validator(orgID) || !contractID || !validator(contractID) || !jobID || !validator(jobID)) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Invalid organization, contract, or job ID'))
+        }
+
+        const expenses = await JobExpense.findAll({
+            where: {
+                organizationId: orgID,
+                contractId: contractID,
+                jobId: jobID
+            },
+            include: [
+                {
+                    model: Job,
+                    where: {
+                        organizationId: orgID,
+                        contractId: contractID,
+                        id: jobID
+                    }
+                }
+            ]
+        })
+
+        return res.status(200).json(createSuccessResponse(expenses))
+    } catch (error) {
+        return res.status(500).json(createErrorResponse(error.message))
+    }
 }
