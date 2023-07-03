f2debfdb1645c2deeefd663d4d5b9594902f2937
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-07-02 23:17:30 -0400

    Update putJobExpense.js
    Updates a job expense for a specific job

--- api/routes/organization/contracts/jobs/putJobExpense.js
+++ api/routes/organization/contracts/jobs/putJobExpense.js
@@ -1,6 +1,44 @@
-module.exports = (req, res) => {
-    res.send({
-        file: __filename.split('/').slice(-1)[0].split('\\').slice(-1)[0],
-        message: 'TODO',
-    })
+const { sequelize, JobExpense, Job } = require('../../../db')
+const { createSuccessResponse, createErrorResponse } = require('../../../utils/response')
+const { validator } = require('../../../utils/validator')
+
+//Update a job expense for a specific job
+module.exports = async (req, res) => {
+    try {
+        const orgID = req.params.org_id;
+        const contractID = req.params.contract_id;
+        const jobID = req.params.job_id;
+        const jobExpenseID = req.params.job_expense_id;
+
+        if (!orgID || !validator(orgID) || !contractID || !validator(contractID) || !jobID || !validator(jobID) || !jobExpenseID || !validator(jobExpenseID)) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Invalid organization, contract, job, or job expense ID'))
+        }
+
+        const { name, description, amount } = req.body;
+
+        if (!name || !validator(name) || !description || !validator(description) || !amount || !validator(amount)) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Invalid name, description, or amount'))
+        }
+
+        const jobExpense = await JobExpense.update({
+            name,
+            description,
+            amount
+        }, {
+            where: {
+                organizationId: orgID,
+                contractId: contractID,
+                jobId: jobID,
+                id: jobExpenseID
+            }
+        })
+
+        return res.status(200).json(createSuccessResponse(jobExpense))
+    } catch (error) {
+        return res.status(500).json(createErrorResponse(error.message))
+    }
 }
