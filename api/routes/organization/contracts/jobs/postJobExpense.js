6bcabd4116ea4ec6de4104e45bb02a0ea6bbd53b
jihee10 <133060345+jihee10@users.noreply.github.com>
2023-07-02 23:14:39 -0400

    Update postJobExpense.js
    fix error, Creates a new job expense for a specific job

--- api/routes/organization/contracts/jobs/postJobExpense.js
+++ api/routes/organization/contracts/jobs/postJobExpense.js
@@ -1,8 +1,8 @@
-const { sequelize, JobMember, Job } = require('../../../db')
+const { sequelize, JobExpense, Job } = require('../../../db')
 const { createSuccessResponse, createErrorResponse } = require('../../../utils/response')
 const { validator } = require('../../../utils/validator')
 
-//Create a new job member for a specific job
+//Create a new job expense for a specific job
 module.exports = async (req, res) => {
     try {
         const orgID = req.params.org_id;
@@ -15,23 +15,24 @@ module.exports = async (req, res) => {
                 .json(createErrorResponse('Invalid organization, contract, or job ID'))
         }
 
-        const { userId } = req.body;
+        const { name, description, amount } = req.body;
 
-        if (!userId || !validator(userId)) {
+        if (!name || !validator(name) || !description || !validator(description) || !amount || !validator(amount)) {
             return res
                 .status(400)
-                .json(createErrorResponse('Invalid user ID'))
+                .json(createErrorResponse('Invalid name, description, or amount'))
         }
 
-        const jobMember = await JobMember.create({
-            userId,
+        const jobExpense = await JobExpense.create({
+            name,
+            description,
+            amount,
             organizationId: orgID,
             contractId: contractID,
             jobId: jobID
         })
 
-        return res.status(200).json(createSuccessResponse(jobMember))
+        return res.status(200).json(createSuccessResponse(jobExpense))
     } catch (error) {
         return res.status(500).json(createErrorResponse(error.message))
     }
-}
