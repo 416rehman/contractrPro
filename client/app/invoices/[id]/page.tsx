774ea1fd6e518ade3021af5a8decd475833f0b27
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-07-16 18:10:36 -0400

    Included the InvoiceForm component to be in the selected invoice's page with styling

---
+++ client/app/invoices/[id]/page.tsx
@@ -0,0 +1,7 @@
+import InvoiceForm from "@/components/invoiceForm";
+
+export default function InvoicePage({ params }: { params: { id: string } }) {
+  return <div className={"flex flex-grow justify-center"}>
+    <InvoiceForm id={params.id} className={"items-center max-w-full sm:max-w-xl"} />
+  </div>;
+}
