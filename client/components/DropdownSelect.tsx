2f8bbe492d51cdbb87025512bb7608c91c52de3f
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-07-15 20:54:20 -0400

    Implemented a dropdown select component

---
+++ client/components/DropdownSelect.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { IconTriangleInvertedFilled } from '@tabler/icons-react';
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
+import { Item } from '@/types';
+
+type Props = {
+    name: string,
+    items: Array<Item>,
+    className?: string,
+}
+
+export function DropdownSelect({name, items, className}: Props) {
+
+    return (
+        <Dropdown className={className}>
+            <DropdownTrigger>
+                <Button size={"lg"} endContent={<IconTriangleInvertedFilled width={16} height={16}/>}>
+                    {name}
+                </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Dynamic Actions" items={items} onAction={(key) => console.log("TODO: Add an action when selecting an item")}>
+                {items.map((item) => (
+                    <DropdownItem key={item.itemKey}>
+                        {item.itemLabel}
+                    </DropdownItem>
+                ))}
+            </DropdownMenu>
+        </Dropdown>
+    );
+}
+
+
