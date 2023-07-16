03a96c6f26fe1c61e9e610ae9911f7bb7d190961
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-07-16 18:06:12 -0400

    Implemented an arrow function expression that will print something to the console log and add a toast that will be executed on action from the DropdownMenu component

--- client/components/DropdownSelect.tsx
+++ client/components/DropdownSelect.tsx
@@ -3,6 +3,7 @@
 import { IconTriangleInvertedFilled } from '@tabler/icons-react';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
 import { Item } from '@/types';
+import { useToastsStore } from '@/services/toast';
 
 type Props = {
     name: string,
@@ -12,15 +13,27 @@ type Props = {
 
 export function DropdownSelect({name, items, className}: Props) {
 
+    const toastsStore = useToastsStore(state => state);
+
+    const selectAction = () => {
+        console.log("TODO: Select Action");
+        toastsStore.addToast({
+          id: "select-action",
+          title: "Select Action",
+          message: "TODO: Not implemented yet",
+          type: "error"
+        });
+    }
+    
     return (
         <div className={className}>
-            <Dropdown className={className}>
+            <Dropdown>
                 <DropdownTrigger>
-                    <Button size={"lg"} endContent={<IconTriangleInvertedFilled width={16} height={16}/>}>
+                    <Button size={"lg"} endContent={<IconTriangleInvertedFilled width={12} height={12}/>}>
                         {name}
                     </Button>
                 </DropdownTrigger>
-                <DropdownMenu aria-label="Dynamic Actions" items={items} onAction={(key) => console.log("TODO: Add an action when selecting an item")}>
+                <DropdownMenu aria-label="Dynamic Actions" items={items} onAction={() => selectAction()}>
                     {items.map((item) => (
                         <DropdownItem key={item.itemKey}>
                             {item.itemLabel}
