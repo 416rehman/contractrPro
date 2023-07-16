5c7b0e9e50228576387849d4ca1f873c3802066e
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-07-16 18:07:54 -0400

    Added the InvoicesSidebar component to be rendered in the invoices page's layout

--- client/app/invoices/layout.tsx
+++ client/app/invoices/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import InvoicesSidebar from "@/components/invoicesSidebar";
 
 export default function DocsLayout({
                                      children
@@ -7,7 +8,8 @@ export default function DocsLayout({
 }) {
   return (
     <section className="flex flex-col justify-center gap-4 flex-grow items-start w-full">
-      <div className="flex flex-grow text-center justify-center w-full">
+      <div className="flex text-center justify-center w-full flex-col h-full sm:flex-row sm:flex-grow">
+        <InvoicesSidebar className={"flex-grow w-full sm:max-w-[15rem]"} />
         {children}
       </div>
     </section>
