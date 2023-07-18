f6375ce2788cb0cba7ff7c2399f4c3340a248154
416rehman <15690606+416rehman@users.noreply.github.com>
2023-07-18 03:59:32 -0400

    fixed imports

--- client/app/contracts/page.tsx
+++ client/app/contracts/page.tsx
@@ -1,9 +1,7 @@
 import { title } from "@/components/primitives";
-import JobScreen from "@/components/jobScreen";
 
 export default function AboutPage() {
   return (<>
-      <JobScreen />
       <section className="flex flex-col flex-grow gap-4 py-8 h-full" id={"contracts"}>
         <h1 className={title()}>Contracts</h1>
       </section>
