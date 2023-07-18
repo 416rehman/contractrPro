20ce030d38fe225ecd75c1460f9c323e08981d6c
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-07-17 21:26:22 -0400

    Added a comment to the component on how it works

--- client/components/employeesSidebar.tsx
+++ client/components/employeesSidebar.tsx
@@ -17,7 +17,7 @@ type Props = {
 
 /**
  * The EmployeesSidebar component renders a sidebar of the list of employees for the user to click on one if they want to view its data in its page.
- * Underneath the list is a button that will allow them to create a new user, taking them to the create page that will allow them to do that.
+ * Underneath the list is a button that will allow them to create a new employee, taking them to the create page that will allow them to do that.
  */
 export default function EmployeesSidebar({ className }: Props) {
   const [currentOrg] = useUserStore(state => [state.currentOrganization]);
