85ac5c644b813a999c8b64cdf882a2e903bc7c63
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-07-21 19:48:41 -0400

    Implemented the route for /organizations/:org_id/vendors/:vendor_id PUT route

---
+++ api/routes/organization/vendors/updateVendor.js
@@ -0,0 +1,57 @@
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../../utils/response')
+const { isValidUUID } = require('../../../utils/isValidUUID')
+const { pick } = require('../../../utils')
+const { sequelize, Vendor } = require('../../../db')
+
+// Updates an organization's vendor by ID
+module.exports = async (req, res) => {
+    try {
+
+        const orgId = req.params.org_id
+        const vendorId = req.params.vendor_id
+
+        if (!orgId || !isValidUUID(orgId)) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Organization ID is required'))
+        }
+        if (!vendorId || !isValidUUID(vendorId)) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Vendor ID is required'))
+        }
+        const body = {
+            ...pick(req.body, [
+                'name',
+                'phone',
+                'email',
+                'website',
+                'description',
+            ]),
+            OrganizationId: orgId,
+            UpdatedByUserId: req.auth.id,
+        }
+        await sequelize.transaction(async (transaction) => {
+            const queryResult = await Vendor.update(body, {
+                where: {
+                    OrganizationId: orgId,
+                    id: vendorId,
+                },
+                transaction,
+                returning: true,
+            })
+            if (!queryResult[0]) {
+                throw new Error('Vendor not found')
+            }
+
+            const updateVendor = queryResult[1][0]
+
+            return res.status(200).json(createSuccessResponse(updateVendor))
+        })
+    } catch (error) {
+        return res.status(400).json(createErrorResponse('', error))
+    }
+}
