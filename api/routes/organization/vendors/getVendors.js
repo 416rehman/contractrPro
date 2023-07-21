9e8184ad4965f7ebae310c2e2956eb40f40bfdbe
Ibrahem Elsawy <ibrahem_e@hotmail.com>
2023-07-21 19:48:25 -0400

    Implemented the route for /organizations/:org_id/vendors GET route

---
+++ api/routes/organization/vendors/getVendors.js
@@ -0,0 +1,34 @@
+const { sequelize, Vendor } = require('../../../db')
+const {
+    createSuccessResponse,
+    createErrorResponse,
+} = require('../../../utils/response')
+const { isValidUUID } = require('../../../utils/isValidUUID')
+
+// Gets all of the organization's vendors
+module.exports = async (req, res) => {
+    try {
+        const orgId = req.params.org_id
+
+        if (!orgId || !isValidUUID(orgId)) {
+            return res
+                .status(400)
+                .json(createErrorResponse('Organization ID is required'))
+        }
+
+        await sequelize.transaction(async (transaction) => {
+            const organizationVendors = await Vendor.findAll({
+                where: {
+                    OrganizationId: orgId,
+                },
+                transaction,
+            })
+
+            return res
+                .status(200)
+                .json(createSuccessResponse(organizationVendors))
+        })
+    } catch (error) {
+        res.status(400).json(createErrorResponse('', error))
+    }
+}
