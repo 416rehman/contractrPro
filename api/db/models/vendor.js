ae21a6de9f2bec6fc37503d575fff25fa4610d11
Ibrahem Elsawy <79288021+IbrahemElsawy@users.noreply.github.com>
2023-07-22 18:30:47 -0400

    Feature/Vendors-Routes (#299)
    * Implemented the route for /organizations/:org_id/vendors POST route
    
    * Implemented the route for /organizations/:org_id/vendors/:vendor_id DELETE route
    
    * Implemented the route for /organizations/:org_id/vendors/:vendor_id GET route
    
    * Implemented the route for /organizations/:org_id/vendors GET route
    
    * Implemented the route for /organizations/:org_id/vendors/:vendor_id PUT route
    
    * Added the vendors routes to be exported as part of the organization routes"
    
    * Implemented unit testing for /organizations/:org_id/vendors POST route
    
    * Implemented unit testing for /organizations/:org_id/vendors/:vendor_id DELETE route
    
    * Implemented unit testing for /organizations/:org_id/vendors/:vendor_id GET route
    
    * Implemented unit testing for /organizations/:org_id/vendors GET route
    
    * Implemented unit testing for /organizations/:org_id/vendors/:vendor_id PUT route
    
    * Apply formatting changes
    
    * Added unique constraints and indexes for the phone and email attributes in the Vendor model
    
    * Apply formatting changes
    
    * Allowed the file to use the organization's vendors router
    
    * Updated the naming
    
    * Updated the naming
    
    * Apply formatting changes
    
    * Updated the name and included a comment
    
    * Switched the value for the phone and email properties in the first test
    
    * Apply formatting changes
    
    * Updated the body to include mock vendor data instead of client when creating a stranger vendor for one of the tests
    
    * Minor grammar update
    
    * Minor grammar update
    
    * Minor grammar update
    
    * Apply formatting changes
    
    * Removed the association between Organization and Vendor (for testing)
    
    * Apply formatting changes
    
    * Added an if statement to check if a Vendor has not been found
    
    * Apply formatting changes
    
    ---------
    
    Co-authored-by: IbrahemElsawy <IbrahemElsawy@users.noreply.github.com>

--- api/db/models/vendor.js
+++ api/db/models/vendor.js
@@ -15,9 +15,11 @@ module.exports.define = (sequelize, DataTypes) => {
             },
             phone: {
                 type: DataTypes.STRING(255),
+                unique: 'organizationConstraint', // Set unique constraint per organization
             },
             email: {
                 type: DataTypes.STRING(255),
+                unique: 'organizationConstraint', // Set unique constraint per organization
             },
             website: {
                 type: DataTypes.STRING(255),
@@ -25,14 +27,22 @@ module.exports.define = (sequelize, DataTypes) => {
             description: {
                 type: DataTypes.STRING(1024),
             },
+        },
+        {
+            indexes: [
+                {
+                    unique: true,
+                    fields: ['OrganizationId', 'email'], // Create index for uniqueness per organization and email
+                },
+                {
+                    unique: true,
+                    fields: ['OrganizationId', 'phone'], // Create index for uniqueness per organization and phone
+                },
+            ],
         }
     )
 
     Vendor.associate = (models) => {
-        Vendor.belongsTo(models.Organization, {
-            onDelete: 'CASCADE',
-        }) // the organization that owns this client
-
         Vendor.hasMany(models.Expense, {
             foreignKey: { allowNull: false },
         })
---
+++ api/tests/unit/organizations/vendors/deleteVendor.test.js
@@ -0,0 +1,87 @@
+const request = require('supertest')
+const app = require('../../../../server')
+const { Organization, Vendor, sequelize } = require('../../../../db')
+const fake = require('../../../../utils/fake')
+
+let orgId, vendorId, strangerVendorId
+
+beforeAll(async () => {
+    const orgResuts = await Organization.findAll()
+    orgId = orgResuts[0].id
+
+    const vendorToDelete = await Vendor.create({
+        ...fake.mockVendorData(),
+        OrganizationId: orgId,
+    })
+    vendorId = vendorToDelete.id
+
+    const strangerVendor = await Vendor.create({
+        ...fake.mockVendorData(),
+        OrganizationId: orgResuts[1].id,
+    })
+    strangerVendorId = strangerVendor.id
+})
+afterAll(async () => {
+    jest.restoreAllMocks()
+    await sequelize.close()
+})
+describe('Delete organization vendor', () => {
+    it('Should delete an organization vendor', async () => {
+        const response = await request(app)
+            .delete(`/organizations/${orgId}/vendors/${vendorId}`)
+            .expect(200)
+
+        expect(response.body.status).toBe('success')
+        expect(response.body.data).toBe(1)
+    })
+
+    it('Should return 400 if organization ID is invalid', async () => {
+        const invalidOrgId = 'invalid-org-id'
+
+        const response = await request(app)
+            .delete(`/organizations/${invalidOrgId}/vendors/${vendorId}`)
+            .expect(400)
+
+        expect(response.body.status).toBe('error')
+    })
+
+    it('Should return 400 if vendor ID is invalid', async () => {
+        const invalidVendorId = 'invalid-vendor-id'
+
+        const response = await request(app)
+            .delete(`/organizations/${orgId}/vendors/${invalidVendorId}`)
+            .expect(400)
+
+        expect(response.body.status).toBe('error')
+    })
+
+    it('Should return 400 if vendor is not found', async () => {
+        jest.spyOn(Vendor, 'destroy').mockImplementationOnce(() => 0)
+
+        const response = await request(app)
+            .delete(`/organizations/${orgId}/vendors/${vendorId}`)
+            .expect(400)
+
+        expect(response.body.status).toBe('error')
+    })
+
+    it('Should return 400 if the vendor is not in the organization', async () => {
+        const response = await request(app)
+            .delete(`/organizations/${orgId}/vendors/${strangerVendorId}`)
+            .expect(400)
+
+        expect(response.body.status).toBe('error')
+    })
+
+    it('Should return 400 if an exception occurs', async () => {
+        jest.spyOn(sequelize, 'transaction').mockImplementationOnce(() => {
+            throw new Error('Something went wrong')
+        })
+
+        const response = await request(app)
+            .delete(`/organizations/${orgId}/vendors/${vendorId}`)
+            .expect(400)
+
+        expect(response.body.status).toBe('error')
+    })
+})
