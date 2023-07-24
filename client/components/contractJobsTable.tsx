3440a560ae430236a3f1bdbae47402e33e41b370
416rehman <15690606+416rehman@users.noreply.github.com>
2023-07-24 17:01:17 -0400

    completed contracts page

---
+++ client/components/contractJobsTable.tsx
@@ -0,0 +1,142 @@
+import React, { useEffect } from "react";
+import { Input, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import { Job } from "@/types";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/dropdown";
+import { Button } from "@nextui-org/button";
+import { IconCurrencyDollar, IconDotsVertical, IconTrash } from "@tabler/icons-react";
+import JobStatusDropdown from "@/components/jobStatusDropdown";
+
+const columns = [
+  { name: "Name", uid: "name" },
+  { name: "Description", uid: "description" },
+  { name: "Start Date", uid: "startDate" },
+  { name: "Due Date", uid: "dueDate" },
+  { name: "Payout", uid: "payout" },
+  { name: "Status", uid: "status" }
+];
+
+type Props = {
+  jobs: Job[];
+  isEditing: boolean;
+  onEntryChanged: (name: string, value: any, id: string) => void;
+  onEntryDeleted: (id: string) => void;
+}
+
+/**
+ * This is a table component for the Contracts page. It displays all the jobs in the contract.
+ * It renders an extra row at the bottom for the user to enter a new job.
+ * On typing in the empty row, a new empty row is created at the bottom.
+ * Also has an action button to delete the row.
+ * The empty row and actions are hidden in edit mode
+ */
+export default function ContractJobsTable({ jobs, isEditing, onEntryChanged, onEntryDeleted }: Props) {
+  const [jobsData, setJobsData] = React.useState(jobs);
+  useEffect(() => {
+    setJobsData(jobs);
+  }, [jobs]);
+
+  const renderCell = (job: Job, columnKey: React.Key, editMode) => {
+    const cellValue = job[columnKey as keyof Job];
+
+    switch (columnKey) {
+      case "name":
+        return <Input aria-label={"Name"}
+                      key={job.id} value={cellValue}
+                      placeholder="Name" variant={"underlined"}
+                      isReadOnly={!editMode}
+                      size={"sm"}
+                      classNames={{
+                        inputWrapper: ["rounded-none"]
+                      }}
+                      onChange={(e) => onEntryChanged(columnKey, e.target.value, job.id)} />;
+      case "description":
+        return <Input aria-label={"Description"}
+                      key={job.id} value={cellValue} placeholder="Description"
+                      variant={"underlined"}
+                      size={"sm"}
+                      isReadOnly={!editMode}
+                      onChange={(e) => onEntryChanged(columnKey, e.target.value, job.id)} />;
+      case "startDate": {
+        // if its a string, convert it to a date
+        const asDate = typeof cellValue === "string" && new Date(cellValue).toISOString().slice(0, -1);
+        return <Input aria-label={"Start Date"}
+                      key={job.id} value={asDate || ""} type="datetime-local"
+                      placeholder="2017-06-01T08:30"
+                      variant={"underlined"}
+                      size={"sm"}
+                      isReadOnly={!editMode}
+                      onChange={(e) => onEntryChanged(columnKey, e.target.value, job.id)} />;
+      }
+
+      case "dueDate": {
+        // if its a string, convert it to a date
+        const asDate = typeof cellValue === "string" && new Date(cellValue).toISOString().slice(0, -1);
+        return <Input aria-label={"Due Date"}
+                      key={job.id} value={asDate || ""} type="datetime-local"
+                      placeholder="2017-06-01T08:30"
+                      variant={"underlined"}
+                      size={"sm"}
+                      isReadOnly={!editMode}
+                      onChange={(e) => onEntryChanged(columnKey, e.target.value, job.id)} />;
+      }
+
+      case "payout":
+        return <Input aria-label={"Payout"}
+                      key={job.id} value={cellValue} type="number" placeholder="Quantity"
+                      variant={"underlined"}
+                      size={"sm"}
+                      isReadOnly={!editMode}
+                      startContent={<IconCurrencyDollar />}
+                      onChange={(e) => onEntryChanged(columnKey, e.target.value, job.id)} />;
+      case "status":
+        const isTouched = columns.some((column) => {
+          return !!job[column.uid];
+        });
+
+        return <div className="relative flex justify-end items-center gap-2">
+          <JobStatusDropdown value={cellValue} onChange={(value) => onEntryChanged(columnKey, value, job.id)} />
+          {editMode && isTouched && <Dropdown>
+            <DropdownTrigger>
+              <Button isIconOnly size="sm" variant="light">
+                <IconDotsVertical className="text-default-300" />
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu onAction={(key) => {
+              if (key === "delete") {
+                onEntryDeleted(job.id);
+              }
+            }}>
+              <DropdownItem startContent={<IconTrash className={"text-default-500"} />}
+                            key={"delete"}>Delete</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>}
+        </div>;
+    }
+  };
+
+  const items = jobs ? [...jobs, { id: Math.random().toString(36).substring(7) }] : [{ id: Math.random().toString(36).substring(7) }];
+
+  return ((
+    <div>
+      <Table aria-label="Contract Jobs" removeWrapper={true}>
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.uid} align={"start"}>
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody>
+          {items.map((item) => {
+            // if its the last item and we are not editing, don't render it
+            if (item.id === items[items.length - 1].id && !isEditing) {
+              return null;
+            } else return <TableRow key={item?.id} className={"p-0"}>
+              {(columnKey) => <TableCell className={"p-0"}>{renderCell(item, columnKey, isEditing)}</TableCell>}
+            </TableRow>;
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  ));
+}
---
+++ client/components/contractsSidebar.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { CardFooter, Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/button";
+import NextLink from "next/link";
+import clsx from "clsx";
+import { IconChartTreemap, IconChevronDown, IconListSearch } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
+import { loadContracts, useContractsStore } from "@/services/contracts";
+import { useUserStore } from "@/services/user";
+import { useParams } from "next/navigation";
+import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/popover";
+
+type Props = {
+  className?: string;
+}
+
+/**
+ * This is the sidebar for the contracts page. It displays a list of contracts and should allow the user to filter them.
+ * It handles communication with the API and updates the local state via the Contract service.
+ * This is used in tandem with the ContractForm component to edit/create contracts.
+ * TODO: Implement filtering
+ */
+export default function ContractsSidebar({ className }: Props) {
+  const [currentOrg] = useUserStore(state => [state.currentOrganization]);
+  const [contracts] = useContractsStore(state => [state.contracts]);
+  const [filter, setFilter] = useState("");
+  const params = useParams();
+
+  useEffect(() => {
+    loadContracts(currentOrg?.id);
+  }, [currentOrg?.id]);
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value);
+    contracts.filter((item: any) => item.name.toLowerCase().includes(e.target.value.toLowerCase()));
+  };
+
+  const sidebar = <Card shadow={"none"} isBlurred={true}
+                        className={clsx("border-none rounded-none", className)}>
+    <CardHeader className={"flex flex-col gap-2"}>
+      <h1 className={"text-2xl font-bold"}>Contracts</h1>
+      {/*  Search bar*/}
+      <Input aria-label={"Filter contracts"}
+             placeholder={"Filter"} size={"sm"} endContent={<IconListSearch className={"text-default-400"} />}
+             variant={"underlined"}
+             onChange={handleFilterChange} />
+    </CardHeader>
+    <CardBody className={"p-2"}>
+      <ul className={"flex flex-col w-full"}>
+        {contracts && contracts.map((contract) => (
+          <li key={contract.id}>
+            <Button
+              className={"w-full justify-start text-default-600 font-medium"}
+              as={NextLink}
+              href={"/contracts/" + contract?.id}
+              startContent={<IconChartTreemap className={"text-default-300"} size={"20"} />}
+              variant={params.id === contract?.id ? "flat" : "light"}
+              size={"sm"}>
+              <span className={"truncate"}>{contract?.name}</span>
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </CardBody>
+    <CardFooter>
+      <Button variant={"ghost"} className={"flex-grow font-md"} href={"/contracts/new"} as={NextLink}>
+        Create New Contract
+      </Button>
+    </CardFooter>
+  </Card>;
+
+  // for mobile version have a dropdown
+  const dropdown = <Popover>
+    <PopoverTrigger className={clsx("w-full flex md:hidden", className)}>
+      <Button variant={"ghost"} className={""} endContent={<IconChevronDown />}>
+        Contracts
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent className={"rounded-md !w-[94vw] !h-[85vh] p-1"}>
+      {sidebar}
+    </PopoverContent>
+  </Popover>;
+
+  return <>
+    <div className={"hidden md:flex md:flex-col md:gap-2 md:w-1/4"}>
+      {sidebar}
+    </div>
+    <div className={"flex md:hidden"}>
+      {dropdown}
+    </div>
+  </>;
+}
