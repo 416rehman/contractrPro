9b54d086019b13636531ef5e3d44ff54aca14766
416rehman <15690606+416rehman@users.noreply.github.com>
2023-07-24 14:58:17 -0400

    enhanced error responses

--- api/utils/response.js
+++ api/utils/response.js
@@ -11,12 +11,17 @@ module.exports.createErrorResponse = (message, err) => {
         // example error: "insert or update on table \"Contracts\" violates foreign key constraint \"Contracts_ClientId_fkey\""
         // We need to get the table name from the error message
         const entity = err.message.split('"')[1]
-        hint = `. Make sure the Id related to ${entity} is valid`
+        hint = `Make sure the Id related to ${entity} is valid`
+    }
+    // null value in column "name" of relation "Jobs" violates not-null constraint
+    else if (err?.message?.includes('violates not-null constraint')) {
+        const column = err.message.split('"')[1]
+        const entity = err.message.split('"')[3]
+        hint = `Make sure "${column}" is not null for "${entity}"`
     }
     return {
         status: 'error',
-        message: message || err?.message || 'Something went wrong',
-        data:
-            (err?.code ? err.code + ': ' + err?.message : err?.message) + hint,
+        message: message || hint || err?.message || 'Something went wrong',
+        data: err?.code ? err.code + ': ' + err?.message : err?.message,
     }
 }
